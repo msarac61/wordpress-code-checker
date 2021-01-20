9e7a7662d49865d16e5dfebac20368d7a1fc87a6
msarac61 <metin_@hotmail.com.tr>
2021-01-20 11:36:00 +0300

    Update Licence

--- inc/metabox.php
+++ inc/metabox.php
@@ -1,83 +1,83 @@
 <?php
 
-// Config Metabox
+	// Config Metabox
 
-function wccheckerMetaBox() {
+	function wccheckerMetaBox() {
 
-	$wccTypes = array( 'sc' ); // Post Type Name
+		$wccTypes = array( 'sc' ); // Post Type Name
 
-	foreach ( $wccTypes as $wccType ) {
+		foreach ( $wccTypes as $wccType ) {
 
-		add_meta_box('wccTypesection', __( 'Wcc Settings', 'wcchecker' ), 'wccheckerMetaBox_callback', $wccType);
+			add_meta_box('wccTypesection', __( 'Wcc Settings', 'wcchecker' ), 'wccheckerMetaBox_callback', $wccType);
 
-	 }
+		 }
 
-}
+	}
 
-add_action( 'add_meta_boxes', 'wccheckerMetaBox' );
+	add_action( 'add_meta_boxes', 'wccheckerMetaBox' );
 
-// Create Metabox Input
+	// Create Metabox Input
 
-function wccheckerMetaBox_callback( $post ) {
+	function wccheckerMetaBox_callback( $post ) {
 
-	wp_nonce_field( 'wccheckerMetaBox_update', 'wccheckerMetaBox_nonce' );
+		wp_nonce_field( 'wccheckerMetaBox_update', 'wccheckerMetaBox_nonce' );
 
-	$value = get_post_meta( $post->ID, 'sc_date', true );
+		$value = get_post_meta( $post->ID, 'sc_date', true );
 
-	echo '<table class="form-table">';
+		echo '<table class="form-table">';
 
-		echo '<tr valign="top">';
+			echo '<tr valign="top">';
 
-			echo '<th scope="row"><label for="wccField">'.__( 'End Time', 'wcchecker' ).'</th> ';
+				echo '<th scope="row"><label for="wccField">'.__( 'End Time', 'wcchecker' ).'</th> ';
 
-			echo '<td><input type="date" id="wccField" name="wccField" value="' . esc_attr( $value ) . '"></td>';
+				echo '<td><input type="date" id="wccField" name="wccField" value="' . esc_attr( $value ) . '"></td>';
 
-		echo '</tr>';
+			echo '</tr>';
 
-	echo '</table>';
+		echo '</table>';
 
-}
+	}
 
-// Metabox Field Update
+	// Metabox Field Update
 
-function wccheckerMetaBox_update( $post_id ) {
+	function wccheckerMetaBox_update( $post_id ) {
 
-	 if ( ! isset( $_POST['wccheckerMetaBox_nonce'] ) ) {
-		return;
-	 }
-
-	 if ( ! wp_verify_nonce( $_POST['wccheckerMetaBox_nonce'], 'wccheckerMetaBox_update' ) ) {
-		return;
-	 }
-
-	 if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	 }
-
-	 // Check the user's permissions.
-	 if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
-
-		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+		 if ( ! isset( $_POST['wccheckerMetaBox_nonce'] ) ) {
 			return;
-		}
+		 }
 
-	 } else {
-
-		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		 if ( ! wp_verify_nonce( $_POST['wccheckerMetaBox_nonce'], 'wccheckerMetaBox_update' ) ) {
 			return;
-		}
-	 }
+		 }
 
-	 if ( ! isset( $_POST['wccField'] ) ) {
-		return;
-	 }
+		 if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		 }
 
-	 $wcc_data = sanitize_text_field( $_POST['wccField'] );
+		 // Check the user's permissions.
+		 if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
 
-	 update_post_meta( $post_id, 'sc_date', $wcc_data );
+			if ( ! current_user_can( 'edit_page', $post_id ) ) {
+				return;
+			}
 
-}
+		 } else {
 
-add_action( 'save_post', 'wccheckerMetaBox_update' );
+			if ( ! current_user_can( 'edit_post', $post_id ) ) {
+				return;
+			}
+		 }
+
+		 if ( ! isset( $_POST['wccField'] ) ) {
+			return;
+		 }
+
+		 $wcc_data = sanitize_text_field( $_POST['wccField'] );
+
+		 update_post_meta( $post_id, 'sc_date', $wcc_data );
+
+	}
+
+	add_action( 'save_post', 'wccheckerMetaBox_update' );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -7,8 +7,8 @@ Requires at least: 3.0.1
 Tested up to: 5.6
 Requires PHP: 5.4
 Stable tag: 4.3
-License: GPL-2.0+
-License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+License: GPL-3.0+
+License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 Wordpress Secure Code Control System
 
 == Description ==
